61140593f0a2b283f2c1deb24e5193e57459e035
Aditya Gupta <adityagupta2087@gmail.com>
2026-04-10 00:40:52 +0530

    removed firebase configs

--- src/pages/dashboard-student.tsx
+++ src/pages/dashboard-student.tsx
@@ -1,5 +1,9 @@
+import { WhenLoggedIn } from "@/components/auth/show";
+
 export default function DashboardStudentPage() {
     return ( 
-        <></>
+        <WhenLoggedIn>
+            <h1>Dashboard</h1>
+        </WhenLoggedIn>
     ); 
 } 
